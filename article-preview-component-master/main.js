88253c9f5713a8cfbe4855ca31a07e1841174cf9
skyfall947 <atencio947@gmail.com>
2021-01-22 19:19:27 -0400

    Little changes make to the 4th challenge
    Chage the border of the text element
    Adding animations for the popUp elements and shadow animation too.

--- article-preview-component-master/main.js
+++ article-preview-component-master/main.js
@@ -1,18 +1,22 @@
-function onClickShare(){
+function onClickShare(e){
     const viewWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-
+    console.log(e);
     if(viewWidth <= 924){
         const footer = document.querySelector('.c-share-profile');
         const footerJs = document.querySelector('.c-share-profile-js');
+        const footerJsContainer = document.querySelector('.c-share-profile-js__container');
         const container = document.querySelector('.c-article__container');
 
         footer.classList.add('o-hidde');
         footerJs.classList.remove('o-hidde');
+        footerJsContainer.classList.add('slide-up-animation');
+
         container.style['padding-bottom'] = 0;
     }else{
         if (this.dataset.state === 'false'){
             const popUp = document.querySelector('.c-share-profile-js-2');
-            popUp.classList.remove('o-hidde');
+
+            popUp.classList.replace('o-hidde', 'slide-up-animation-2');
             this.dataset.state = 'true';
         }else{
             const popUp = document.querySelector('.c-share-profile-js-2');
